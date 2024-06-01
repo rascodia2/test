5ca1b8dc87fd89a1f0c659891e309a32c3d71fb8
rascodia2 <oscaraguilera_16@hotmail.com>
2024-06-01 10:48:19 -0400

    ejercicio frase y sets

---
+++ frase.py
@@ -0,0 +1,4 @@
+
+frase = input("Ingrese una frase")
+
+print("La cantidad de las palabras es: ", len(set(frase.split())))
--- tuplas_sets.py
+++ tuplas_sets.py
@@ -1,5 +1,5 @@
 
-
+tupla_0 = ("Javier","Oscar","Pedro")
 list = ["manzana", "pera"]
 
 list.append("naranja")
@@ -24,3 +24,54 @@ print("Lista 2", lista_2)
 ##Podemos recorrer una tupla igual que una lista
 for fruta in tuple_1:
     print(fruta)
+
+print(tuple_1[1:4])
+print(tupla_0)
+
+tupla_4 = tupla_0 + tuple_1
+print(tupla_4)
+#Contando la cantidad de veces que aperece un elemento en la tupla
+print(tupla_4.count("manzana"))
+
+#Muestra el índicia (posición) donde se encuentra el elemento buscado
+print(tupla_4.index("Pedro"))
+
+print((tupla_0,tupla_4))
+
+###############################################
+###                 SETS                    ###
+###############################################
+
+thisset = {"apple","cherry", "banana",  "apple"}
+
+set_2 = thisset
+
+print(set_2)
+
+set_2.add("pineapple")
+print(set_2)
+set_2.add("pineapple")
+print(set_2)
+
+for fruit in set_2:
+    print(fruit)
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya","apple"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+thisset.remove("banana")
+
+print(thisset)
+
+matrix_set = thisset
+
+print(matrix_set)
+
+set_de_lista = set(lista_2)
+
+print(set_de_lista)
+
